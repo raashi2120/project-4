edd4ed4696cf558cdfc5e1d5cc1e018265ef923b
Raashi Sharma <sharmaraashi21@gamil.com>
2025-07-11 14:50:16 +0530

    made products pages for all categories

--- index.js
+++ index.js
@@ -13,6 +13,17 @@ app.get("/", (req, res) => {
     res.render('home', { ...data });
 })
 
+app.get("/:prod", (req, res) => {
+    const { prod } = req.params;
+    const product = data[`${prod}`];
+
+    res.render('products', { product });
+
+
+})
+
+
+
 app.listen(3000, () => {
     console.log("listening on port 3000");
 })
---
+++ views/script.js
@@ -0,0 +1,1 @@
+const makeup = document.querySelector("#m");
